27fd5b37b806a110d3dd2bbd703f530e576c9dc0
Danendra Indiarto <indiartodanendra@gmail.com>
2026-01-28 08:08:10 +0700

    add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { PrismaModule } from './prisma/prisma.module';
 import { BooksModule } from './books/books.module';
 import { MembersModule } from './members/members.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { LoanModule } from './loan/loan.module';
 import { LoanDetailModule } from './loandetail/loan-detail.module';
 import { AuthModule } from './auth/auth.module';
@@ -11,6 +14,14 @@ import { ReturnModule } from './return/return.module';
 
 @Module({
   imports: [
+    // 🌍 ENV CONFIG (dev & production)
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
+    }),
+
+    // 📦 APP MODULES
     PrismaModule,
     BooksModule,
     MembersModule,
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Library API')
+    .setDescription('Backend API Sistem Perpustakaan')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
-void bootstrap();
+bootstrap();
