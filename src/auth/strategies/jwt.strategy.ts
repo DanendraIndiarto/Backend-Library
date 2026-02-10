eca7fd374e0db26f3b59771cbf24ed9c3f1bdf54
Danendra Indiarto <indiartodanendra@gmail.com>
2026-02-10 14:56:42 +0700

    upgrade jwt.strategy.ts part 2

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,24 +1,15 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private config: ConfigService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: config.get<string>('JWT_SECRET'), // 🔥 HARUS SAMA
+      secretOrKey: 'SECRET_KEY',
     });
   }
-
   validate(payload: any) {
-    return {
-      userId: payload.sub,
-      username: payload.username,
-      role: payload.role, // 🔥 INI YANG DIPAKAI RolesGuard
-      memberId: payload.memberId,
-    };
+    return payload;
   }
 }
